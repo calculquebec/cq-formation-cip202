0ab9b34231e57cf6ed89fe32cfe301a8f1b64bc8
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2025-03-03 17:17:50 -0500

    Fichiers du second exercice GNU Parallel

---
+++ lab/bio-info/blastn-parallel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=X
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --time=0-00:10
+
+module load StdEnv/2023 gcc/12.3 blast+/2.14.1 seqkit/2.5.1
+
+mkdir -p res_prll
+
+parallel 'blastn -db spec_{} -query chr_{}.fa > res_prll/align_{}_{}' \
+   A B C D  K L M N O P Q R S T U V W X Y Z
