1378e3a3882e9357ed76d28d241bcb2c9cc347cb
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2025-03-07 14:44:48 -0500

    Correction du répertoire créé

--- lab/bio-info/solution/blastn-parallel.sh
+++ lab/bio-info/solution/blastn-parallel.sh
@@ -5,7 +5,7 @@
 
 module load StdEnv/2023 gcc/12.3 blast+/2.14.1 seqkit/2.5.1
 
-mkdir -p res_prll
+mkdir -p results
 
-parallel 'blastn -db spec_{1} -query chr_{2}.fa > res_prll/align_{2}_{1}' \
+parallel 'blastn -db spec_{1} -query chr_{2}.fa > results/align_{2}_{1}' \
   ::: A B C D ::: K L M N O P Q R S T U V W X Y Z
